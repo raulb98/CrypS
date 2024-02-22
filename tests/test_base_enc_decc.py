0c9604543c4dd0027a9e8cbb8c530805e68165d8
raulb98 <52500444+raulb98@users.noreply.github.com>
2024-02-22 23:38:50 +0200

    Added colors to Unitest

--- tests/test_base_enc_decc.py
+++ tests/test_base_enc_decc.py
@@ -3,6 +3,17 @@ import os
 sys.path.insert(0, os.path.abspath('..'))
 from BaseEncDec.base_enc_dec import BaseEncDec
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def testing_base_enc(cls): 
     class TestDecorator: 
         def __init__(self, x):
@@ -10,13 +21,12 @@ def testing_base_enc(cls):
         def test_encode(self, string, rez): 
             encoded_string = self.wrap.base_enc_dec.encode(string)
             assert(encoded_string == rez)
-            print("Passed")
+            print("Passed" + bcolors.OKGREEN)
         def test_decode(self, string,rez):
             encoded_string = self.wrap.base_enc_dec.decode(string)
             assert(encoded_string == rez)
-            print("Passed")
-    return TestDecorator 
-
+            print("Passed" + bcolors.OKGREEN)
+    return TestDecorator
 
 @testing_base_enc
 class TestBaseEncDec():
